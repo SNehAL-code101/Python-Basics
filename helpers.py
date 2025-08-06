94d5fb798c5056db8eb049371c237bda294dc27b
SnehalRajapurkar <66864498+SNehAL-code101@users.noreply.github.com>
2025-08-06 21:39:44 +0530

    Add files via upload

---
+++ helpers.py
@@ -0,0 +1,66 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "332c510a-a03b-4d4d-bd46-33e25d3f5134",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#Function to return launch codes\n",
+    "def launch_codes():\n",
+    "    return 123456789"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "dd359d51-1757-4fab-9d56-e683382acaa9",
+   "metadata": {},
+   "outputs": [
+    {
+     "data": {
+      "text/plain": [
+       "123456789"
+      ]
+     },
+     "execution_count": 4,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "launch_codes()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "66a663bb-98f3-45ae-ab9b-c9cab22b791f",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
